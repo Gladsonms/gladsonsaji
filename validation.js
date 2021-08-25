9930ae6b0e04ad487e4da7e09f873a7f378d79b3
Gladsonms <gladsonsaji4@gmail.com>
2021-08-25 17:04:42 +0530

    added mail

--- validation.js
+++ validation.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     $('#name').on('input',function () {
       validateusername();
     })
+    var validname=false;
    function validateusername ()
    {
      let namevalue = $('#name').val();
@@ -12,18 +13,24 @@ $(document).ready(function() {
        $('#Errorname').show();
        $('#Errorname').html("2 consicutive space are not allowed")
        Errorname = false;
+       validname=false;
        return false;
+
      }
      else if(!namevalue.match(nameRegex))
      {
          $("#Errorname").show();
          $("#Errorname").html("Name contain only alphabets ")
+         Errorname = false;
+         validname=false;
+         return false;
      }
      else if(namevalue.length=0)
      {
          $('#Errorname').show();
          $('#Errorname').html("name field cant be empty");
          Errorname=false;
+         validname=false;
          return false;
 
      }
@@ -32,14 +39,17 @@ $(document).ready(function() {
             $('#Errorname').show()
             $('#Errorname').html("Please enter a valid name ");
             Errorname=false;
+            validname=false;
             return false;
     
      }
      else
      {
          $('#Errorname').hide();
+         validname=true;
      }
    }
+   var validmail=false
   $('#Errormail').hide();
   let Errormail=true;
   $('#email').on('input',function(){
@@ -54,6 +64,7 @@ $(document).ready(function() {
            $('#Errormail').html("Mail cant be empty");
            Errormail=false;
            return false;
+           validmail=false;
 
        }
        else if(!mailValue.match(regexEmail))
@@ -62,39 +73,48 @@ $(document).ready(function() {
         $('#Errormail').html("Not an email format");
         Errormail=false;
         return false;
+        validmail=flase;
        }
        else {
            $('#Errormail').hide();
+           validmail=true;
        }
    }
    $('#Errorphone').hide();
    let Errorphone=true;
-   $('#phone').on('input',function(){
+   var validphone=false;
+   $('#phone').on('input',function(e){
        validatePhone();
    })
        function validatePhone(){
            let phonevalue=$("#phone").val();
+          
            if(phonevalue.length!=10)
            {
                $('#Errorphone').show();
                $('#Errorphone').html("Please enter a valid number")
+               validphone=false;
                Errorphone=false;
                return false;
            }
+           
            else if(isNaN(phonevalue))
            {
             $('#Errorphone').show();
             $('#Errorphone').html("Please enter a valid number")
+            validphone=false;
             Errorphone=false;
             return false;
            }
            else{
                $('#Errorphone').hide();
+               validphone=true;
            }
 
        }
    $('#ErrorMessage').hide();
      let ErrorMessage=true;
+     var validMessage=false;
      $('#message').on('input',function(){
          validateMeassage();
      })
@@ -106,13 +126,46 @@ $(document).ready(function() {
              $("#ErrorMessage").html("Please enter atleast 15 charcaters");
              ErrorMessage=false;
              return false;
+             validMessage=false
          }
          else
          {
-             $('#ErrorMessage').hide();
+            $('#ErrorMessage').hide();
+            validMessage=true;
          }
      }
+
+     $('#submit-form').submit((e)=>{
+        e.preventDefault()
+        if(validname==true && validphone==true && validmail==true && validMessage==true){
+            $('#ErrMsg').text("")
+            $.ajax({
+                
+                url:"https://script.google.com/macros/s/AKfycbxqeL4Dw3JtyLmmzW0AXjJDTxQtmQ8Sc0ZGTn_o2raFt_gNhJUOrJcW4l8XEfRY5byu/exec",
+                data:$("#submit-form").serialize(),
+                method:"post",
+                success:function (response){
+                    alert("Form submitted successfully")
+                    window.location.reload()
+                    //window.location.href="https://google.com"
+                },
+                error:function (err){
+                    alert("Something Error")
+    
+                }
+            })
+        }
+        else
+        {
+           console.log("ihi")
+            $('#ErrMsg').text("Please enter all fields")
+            console.log("error")
+        }
+        
+     })
      
+     
+   
   })
 
    
